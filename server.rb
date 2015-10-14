00d999b697c6d9f7df500ba1518eeba6a145dbf7
tsuchihasi <tsuchihasi@coiney.com>
2015-10-14 16:55:43 +0900

    https -> git@

--- server.rb
+++ server.rb
@@ -8,21 +8,23 @@ post '/webhook' do
     read = JSON.parse(request.body.read)
     reponame = "#{read['repository']['name']}"
     ref = "#{read['ref']}"
-    svnurl = "#{read['repository']['svn_url']}"
+    sshurl = "#{read['repository']['ssh_url']}"
     event = "#{request.env.select { |k, v| k.start_with?('HTTP_X_GITHUB_EVENT') }}"
     wiki = event.include?("gollum")
     ua = "#{request.user_agent}"
     check = ua.include?("GitHub-Hookshot")
+    wikiurl = sshurl[1,15] + "#{read['repository']['full_name']}" + ".wiki.git"
+    puts wikiurl
 
     if check then
       if wiki == false
-        `git clone #{svnurl}.git /tmp/webhook/#{reponame}
+        `git clone #{sshurl} /tmp/webhook/#{reponame}
         cd /tmp/webhook/#{reponame}
         git remote add downstream https://github.com/#{DOWNSTREAM}/#{reponame}
         git push downstream #{ref}:#{ref}
         rm -rf ../#{reponame}`
       else
-        `git clone #{svnurl}.wiki.git /tmp/webhook/#{reponame}.wiki
+        `git clone #{wikiurl} /tmp/webhook/#{reponame}.wiki
         cd /tmp/webhook/#{reponame}.wiki
         git remote add downstream https://github.com/#{DOWNSTREAM}/#{reponame}.wiki
         git push downstream #{ref}:#{ref}
